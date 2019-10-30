2b8124b5fca109122f2612be0df32bea1b0badf4
ShobhinShaji <57086082+ShobhinShaji@users.noreply.github.com>
2019-10-30 15:25:49 +0530

    Add files via upload

---
+++ VendorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrudOperation.Models;
+using Microsoft.AspNetCore.Mvc;
+using static CrudOperation.Models.VendorRepository;
+
+namespace CrudOperation.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Vendor")]
+    [ApiController]
+    public class VendorController : ControllerBase
+    {
+        private readonly IVendorRepository _gameRepository;
+        public VendorController(IVendorRepository gameRepository)
+        {
+            _gameRepository = gameRepository;
+        }
+      
+
+        [HttpPost]
+        public async Task<IActionResult> post([FromBody]Vendor vender)
+        {
+            await _gameRepository.Create(vender);
+            return new OkObjectResult(vender);
+        }
+       
+
+    }
+}
